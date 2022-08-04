dca3bfd9a22c675c35ca7af5aab96697d8cd0553
VIJENDRASANODIYA <101079329+VIJENDRASANODIYA@users.noreply.github.com>
2022-08-04 00:38:10 -0400

    Add files via upload

---
+++ august 1 week/variable.js
@@ -0,0 +1,48 @@
+var a=20;
+console.log(a);
+var name="vijendra"
+console.log(name);
+
+var age=20;
+console.log(age);
+
+var gendar="male"
+console.log(gendar);
+
+var clg="tit"
+console.log(clg);
+
+var acadmy="geekster"
+console.log(acadmy);
+
+var lastname="sanodiya"
+console.log(lastname);
+
+var dob=29052001
+console.log(dob);
+
+var add="bamhodi"
+console.log(add);
+
+var work="software engineer"
+console.log(work);
+
+var s="jh"
+console.log(s);
+
+var yup="aatma"
+console.log(yup);
+
+var item="mor"
+console.log(item);
+
+var mind="sharp"
+console.log(mind);
+
+
+var day ="monday"
+console.log(day);
+
+var month="july"
+console.log(month);
+
