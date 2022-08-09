2569eed29c3ef68b86ae90afe0a6fd18b04fab8d
VIJENDRASANODIYA <101079329+VIJENDRASANODIYA@users.noreply.github.com>
2022-08-09 12:56:27 -0400

    08 august 22
    fassignment 01

---
+++ assignment.js
@@ -0,0 +1,92 @@
+let product=[
+    {
+        name: "vijendra",
+        gender:"male",
+        id: 101,
+        city:"mumbai",
+    },
+    {
+        name: "vijay",
+        gender:"male",
+        id: 10160,
+        city:"banglore",
+    },
+    {
+        name: "s1",
+        gender:"female",
+        id: 10121,
+        city:"hydrabad",
+    },
+    {
+        name: "ashu",
+        gender:"male",
+        id: 101,
+        city:"kalkuta",
+    },
+    {
+        name: "shivami",
+        gender:"female",
+        id: 10981,
+        city:"noida",
+    },
+    {
+        name: "kasish",
+        gender:"female",
+        id: 10187,
+        city:"prayagraj",
+    },
+    {
+        name: "mansi",
+        gender:"female",
+        id: 100901,
+        city:"pune",
+    },
+]
+//question 1-- print those student who belong hydrabad
+// let res=product[2]
+// console.log(res);
+
+// =====================================================================
+//  question  2--print only male student
+
+// console.log(product[0]);
+// console.log(product[1]);
+// console.log(product[3]);
+
+// =====================================================================
+
+// question  3----print only female studnet
+
+// console.log(product[2]);
+// console.log(product[4]);
+// console.log(product[5]);
+// console.log(product[6]);
+
+// ======================================================================
+
+// question 4---print those student who belong to city hyd and pune
+
+// console.log(product[2]);
+// console.log(product[6]);
+
+// ======================================================================
+
+// qustion 5---print all the student name city
+
+// console.log(product[0].name ,product[0].city);
+// console.log(product[1].name ,product[1].city);
+// console.log(product[2].name ,product[2].city);
+// console.log(product[3].name ,product[3].city);
+// console.log(product[4].name ,product[4].city);
+// console.log(product[5].name ,product[5].city);
+// console.log(product[6].name ,product[6].city);
+
+
+
+
+
+
+
+
+
+
