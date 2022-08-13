70bc5d6b7bc89f773e8158053128885c3f9c3e94
vijendra sanodiya <sanodiyavijendra65@gmail.com>
2022-08-13 00:14:30 -0400

    commit git

---
+++ Assignment4.js
@@ -0,0 +1,10 @@
+var studentsMarks = [40,50,60,79,21,95,35,95,05,12]
+
+// wap the element between 30 and 80
+
+studentsMarks.forEach(function(element,idex){
+    if(element>30  && element<80){
+        console.log(element);
+    }
+
+})
---
+++ Assignment5.js
@@ -0,0 +1,10 @@
+var studentsMarks = [40,50,60,79,21,95,35,95,05,12]
+
+// wap the elementwhich less than  30 
+
+studentsMarks.forEach(function(element,idex){
+    if(element<30){
+        console.log(element);
+    }
+
+})
